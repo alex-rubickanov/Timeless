e9d659b789239e27ef5ef0515d3e73aefc78bafd
Alexander Miroshnichenko <a.miroshnichenko@student.saedubai.com>
2023-08-02 01:31:13 +0400

    Added level

--- TimelessProject/Assets/Scripts/Alex/Player/CameraBehaviour.cs
+++ TimelessProject/Assets/Scripts/Alex/Player/CameraBehaviour.cs
@@ -16,22 +16,26 @@ public class CameraBehaviour : MonoBehaviour
     [SerializeField] private bool useClamp;
     [SerializeField] private float minClampZPos;
     [SerializeField] private float maxClampZPos;
+    [SerializeField] private float minClampXPos;
+    [SerializeField] private float maxClampXPos;
 
+    private float fixedYPosition;
 
     private void Start()
     {
         transform.position = target.position + positionOffset; 
         transform.eulerAngles = new Vector3(cameraAngleX, 0, 0 );
+        fixedYPosition = target.position.y + positionOffset.y;
     }
 
     private void LateUpdate()
     {
         float smoothFollow = (float)cameraSpeed / 1000;
 
-        Vector3 targetPosition = target.position + positionOffset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothFollow);
+        Vector3 requiredPosition = target.position + positionOffset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, requiredPosition, smoothFollow);
         transform.position = useClamp ? 
-            new Vector3(smoothedPosition.x, smoothedPosition.y, Mathf.Clamp(smoothedPosition.z, minClampZPos, maxClampZPos)) : smoothedPosition;
+            new Vector3(Mathf.Clamp(smoothedPosition.x, minClampXPos, maxClampXPos), fixedYPosition, Mathf.Clamp(smoothedPosition.z, minClampZPos, maxClampZPos)) : smoothedPosition;
         
     }
 }
