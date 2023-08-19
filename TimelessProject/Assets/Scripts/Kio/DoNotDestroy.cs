15e7bbe11f75e4f25b969620928b91966d7450f1
Alexander Miroshnichenko <a.miroshnichenko@student.saedubai.com>
2023-08-19 18:23:36 +0400

    Fixed bug with saved volume

--- TimelessProject/Assets/Scripts/Kio/DoNotDestroy.cs
+++ TimelessProject/Assets/Scripts/Kio/DoNotDestroy.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class DoNotDestroy : MonoBehaviour
 {
+    private AudioSource audioSource;
+    
     private void Awake()
     {
+        audioSource = GetComponent<AudioSource>();
+        
         GameObject[] musicObj = GameObject.FindGameObjectsWithTag("BGM");
 
         if (musicObj.Length > 1)
         {
             Destroy(this.gameObject);
         }
+
+        audioSource.volume = PlayerPrefs.GetFloat("volume");
         
         DontDestroyOnLoad(this.gameObject);
     }
